70753d5b79004b75e00bca96f6b5cb1435d6be5c
Kovács Veronika <Kovcs Veronika@192.168.0.227>
2017-10-17 23:11:52 +0200

    Modell generátor első verzió.

--- src/main/java/learning/Main.java
+++ src/main/java/learning/Main.java
@@ -1,5 +1,7 @@
 package learning;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import dal.TestMaker;
@@ -8,21 +10,50 @@ import dal.TrainerTagMaker;
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			System.out.println("Start...");
-			TrainerTagMaker.make(
-					"C:\\Users\\Danasyo\\Desktop\\TrainNER.csv",
-					"C:\\Users\\Danasyo\\Desktop\\TagedTrainNER.csv");
-			TestMaker.make(
-					"C:\\Users\\Danasyo\\Desktop\\Test1NER.csv",
-					"C:\\Users\\Danasyo\\Desktop\\PreparedTest1NER.csv");
-			TestMaker.make(
-					"C:\\Users\\Danasyo\\Desktop\\Test2NER.csv",
-					"C:\\Users\\Danasyo\\Desktop\\PreparedTest2NER.csv");
-			System.out.println("...End");
-		} catch (IOException e) {
-			e.printStackTrace();
+		
+		System.out.println("Start...");
+		
+		File f = new File("src/main/prepared/TagedTrainNER.txt");
+		if(!f.exists()) { 
+			try {
+				TrainerTagMaker.make(
+						"src/main/resources/TrainNER.csv",
+						"src/main/prepared/TagedTrainNER.txt");
+				System.out.println("Prepared trainer data created.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
+		
+		f = new File("src/main/prepared/PreparedTest1NER.txt");
+		if(!f.exists()) { 
+			try {
+				TestMaker.make(
+						"src/main/resources/Test1NER.csv",
+						"src/main/prepared/PreparedTest1NER.txt");
+				System.out.println("Prepared test1 data created.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		f = new File("src/main/prepared/PreparedTest2NER.txt");
+		if(!f.exists()) { 
+			try {
+				TestMaker.make(
+						"src/main/resources/Test2NER.csv",
+						"src/main/prepared/PreparedTest2NER.txt");
+				System.out.println("Prepared test2 data created.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		GenerateModel model = new GenerateModel();
+		model.TrainModel("src/main/prepared/TagedTrainNER.txt");
+
+		System.out.println("...End");
 
 	}
 
